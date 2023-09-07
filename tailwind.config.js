5951c297b545ce03a4de8fa5d251e35f2b4f8ec5
Daniele Rolli <danielerolli004@gmail.com>
2023-09-07 21:38:57 +0200

    🏗️ Updated for version 0.0.8 of Beaver Notes
    What has changed:
    
    1. Blog redesign
    2. Update to the download page
    3. Update to the docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     theme: {
+      container: {
+        // default breakpoints but with 40px removed
+        screens: {
+          sm: '600px',
+          md: '728px',
+          lg: '984px',
+          xl: '1240px',
+          '2xl': '1496px',
+        },
+      },
       extend: {
         objectPosition: {
           'center-top': 'center top',
