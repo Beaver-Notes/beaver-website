e9fd6c4634a8494b04c9e1424e7fb7430c67367b
Daniele Rolli <danielerolli@proton.me>
2025-02-24 08:29:45 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ const mobileMenuOpen = ref(false);
 const navigation = [
   { name: "Home", href: "#/" },
   { name: "Download", href: "#/Download" },
-  { name: "Github", href: "https://blog.beavernotes.com" },
+  { name: "Github", href: "https://github.com/Beaver-Notes/Beaver-Notes" },
   {
     name: "Docs",
     href: "https://docs.beavernotes.com",
